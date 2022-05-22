3f15d7cf943547afb40bb337a9f2730991dd5036
Valtenis Souza <valtenis.souza.1@gmail.com>
2022-05-22 10:28:11 -0300

    Removido resposta ao listar

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -6,7 +6,8 @@ export class List {
     data: SlashCommandBuilder = new SlashCommandBuilder().setName('list').setDescription('Lista os horários dos boss!');
 
     async execute(interaction: CommandInteraction): Promise<void> {
-      await interaction.reply({content: 'eae', fetchReply: true, ephemeral: true})
-		  await mostrarHorarios(interaction.channel);
+        await interaction.deferReply()
+        await interaction.deleteReply()
+		await mostrarHorarios(interaction.channel);
     }
 }
