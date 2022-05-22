781a6088cafa065c6b9723301b2af04664d2e592
Valtenis Souza <valtenis.souza.1@gmail.com>
2022-05-22 10:52:26 -0300

    Adicionado dica para os comandos ao listar

--- src/templates/tabela-horario-boss.ts
+++ src/templates/tabela-horario-boss.ts
@@ -10,11 +10,14 @@ const mostrarHorarios = async (textChannel: TextBasedChannel | null) => {
         const embedTabelaBoss = new MessageEmbed()
             .setColor('#0099ff')
             .setTitle('Tabela de Horários Boss')
-            .setDescription('\u200B');
+            .setDescription('\u200B')
+            .setFooter({ text: "Para listar horários: /list\nPara adicionar novo horário: /add", iconURL: 'https://i.imgur.com/VzgX7yd.jpg' });
 
         listaBoss.forEach((boss: Boss) => {
             embedTabelaBoss.addField(boss.nome, formatBoss(boss), true);
         });
+
+        embedTabelaBoss.addField('\u200B', '\u200B');
 
         await textChannel?.send({ embeds: [embedTabelaBoss] });
     });
