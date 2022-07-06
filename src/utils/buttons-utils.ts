cf52be2412c87329a280c8c87aea2b850b660cd4
Valtenis Souza <valtenis.souza.1@gmail.com>
2022-07-05 22:09:49 -0300

    Removido estado desabilitado do botão próximos

--- src/utils/buttons-utils.ts
+++ src/utils/buttons-utils.ts
@@ -1,9 +1,10 @@
 import { MessageActionRow, MessageButton } from "discord.js";
+import { Ids } from "../models/ids";
 
 const disableButton = (buttons: MessageButton[], idToDisable: string): MessageActionRow => {
     buttons.forEach((button: MessageButton) => {
         const isSelected: boolean = button.customId === idToDisable;
-        button.setDisabled(isSelected);
+        button.setDisabled(isSelected && idToDisable !== Ids.BUTTON_TABLE_PROXIMOS);
         button.setStyle(isSelected ? 'PRIMARY' : 'SECONDARY');
     });
 
