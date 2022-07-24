a94b6a501049d2be1fb2269be7368dcf4545f0e5
Valtenis Souza <valtenis.souza.1@gmail.com>
2022-07-23 22:41:10 -0300

    Adicionado ícones nos botões

--- src/templates/buttons/proximos-buttons.ts
+++ src/templates/buttons/proximos-buttons.ts
@@ -5,11 +5,13 @@ const getButtonsProximos = (): MessageButton[] => {
 
     const buttonProximoAbrir = new MessageButton()
         .setCustomId(Ids.BUTTON_ABRIR_PROXIMOS)
+        .setEmoji('✅')
         .setLabel('Vai Abrir')
         .setStyle('SUCCESS');
 
     const buttonProximoFechar = new MessageButton()
         .setCustomId(Ids.BUTTON_FECHAR_PROXIMOS)
+        .setEmoji('🏴‍☠️')
         .setLabel('Vai Fechar')
         .setStyle('DANGER');
 
--- src/templates/buttons/style-tabela-buttons.ts
+++ src/templates/buttons/style-tabela-buttons.ts
@@ -5,26 +5,31 @@ const getButtonsTabela = (): MessageButton[] => {
 
     const buttonTableBoss = new MessageButton()
         .setCustomId(Ids.BUTTON_TABLE_BOSS)
+        .setEmoji('🔥')
         .setLabel('Todos')
         .setStyle('SECONDARY');
 
     const buttonTableSala = new MessageButton()
         .setCustomId(Ids.BUTTON_TABLE_SALA)
+        .setEmoji('💢')
         .setLabel('Salas')
         .setStyle('SECONDARY');
 
     const buttonTableProximos = new MessageButton()
         .setCustomId(Ids.BUTTON_TABLE_PROXIMOS)
+        .setEmoji('⏭')
         .setLabel('Próximos')
         .setStyle('SECONDARY');
 
     const buttonTableRank = new MessageButton()
         .setCustomId(Ids.BUTTON_TABLE_RANK)
         .setEmoji('🏆')
+        .setLabel('Rank')
         .setStyle('SECONDARY');
 
     const buttonTableHistorico = new MessageButton()
         .setCustomId(Ids.BUTTON_TABLE_HISTORICO)
+        .setEmoji('💾')
         .setLabel('Histórico')
         .setStyle('SECONDARY');
 
