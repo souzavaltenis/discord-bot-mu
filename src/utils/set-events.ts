ad7fd620a5e9ece427f8bd3bcc6cea57fa686b2c
Valtenis Souza <valtenis.souza.1@gmail.com>
2022-08-12 19:59:59 -0300

    Desabilitado sistema de cargos horários

--- src/utils/set-events.ts
+++ src/utils/set-events.ts
@@ -82,22 +82,22 @@ const setEvents = (): void => {
     });
 
     client.on('voiceStateUpdate', async (oldState: VoiceState, newState: VoiceState) => {
-        // Connect main channel
-        if(oldState.channelId === null && newState.channelId === config().channels.voiceHorarios) {
-            await oldState.member?.roles.add(config().cargos.horarios);
-        }
-        // Disconect main channel
-        if (newState.channelId === null && oldState.channelId === config().channels.voiceHorarios) {
-            await newState.member?.roles.remove(config().cargos.horarios);
-        }
-        // Move from main channel to other channel
-        if(oldState.channelId === config().channels.voiceHorarios && newState.channelId !== config().channels.voiceHorarios) {
-            await newState.member?.roles.remove(config().cargos.horarios);
-        }
-        // Move from other channel to main channel
-        if(oldState.channelId !== config().channels.voiceHorarios && newState.channelId === config().channels.voiceHorarios) {
-            await oldState.member?.roles.add(config().cargos.horarios);
-        }
+        // // Connect main channel
+        // if(oldState.channelId === null && newState.channelId === config().channels.voiceHorarios) {
+        //     await oldState.member?.roles.add(config().cargos.horarios);
+        // }
+        // // Disconect main channel
+        // if (newState.channelId === null && oldState.channelId === config().channels.voiceHorarios) {
+        //     await newState.member?.roles.remove(config().cargos.horarios);
+        // }
+        // // Move from main channel to other channel
+        // if(oldState.channelId === config().channels.voiceHorarios && newState.channelId !== config().channels.voiceHorarios) {
+        //     await newState.member?.roles.remove(config().cargos.horarios);
+        // }
+        // // Move from other channel to main channel
+        // if(oldState.channelId !== config().channels.voiceHorarios && newState.channelId === config().channels.voiceHorarios) {
+        //     await oldState.member?.roles.add(config().cargos.horarios);
+        // }
         // If mute audio on main channel, move to afk channel
         if (!newState.member?.roles.cache.has(config().cargos.headset) && newState.selfDeaf && newState.channelId === config().channels.voiceHorarios) {
             await newState.member?.voice.setChannel(config().channels.voiceAfk);
